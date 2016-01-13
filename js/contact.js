c92bdb257b184396386a2343679d5da7992fdfae
Jordan Barnes <rjordanbarnes@gmail.com>
2016-01-12 18:24:29 -0800

    Reorganized Contact page and added pictures

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,14 @@
 var main = function() {
     $(".contact-info-portrait").mouseenter(function() {
-    	$(this).css("background-color", "#f6a6c1");
-    	$(this).children(".contact-info-container").slideDown(200)
+        var classBackground = $(this).children(".contact-info-container").attr("class").split(" ")[1];
+        $(this).addClass(classBackground);
+    	$(this).css("background-color", classBackground);
+    	$(this).children(".contact-info-container").slideDown(200);
     });
 
     $(".contact-info-portrait").mouseleave(function() {
-    	$(this).css("background-color", "initial");
+        var classBackground = $(this).children(".contact-info-container").attr("class").split(" ")[1];
+        $(this).removeClass(classBackground);
     	$(this).children(".contact-info-container").slideUp(100);
     });
 };
