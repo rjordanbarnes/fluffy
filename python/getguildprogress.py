1ff187517a4e287e2b8bff8d3ab63968979d2c9e
Jordan Barnes <rjordanbarnes@gmail.com>
2016-09-30 11:01:46 -0700

    Future proofed the Progress script

--- python/getguildprogress.py
+++ python/getguildprogress.py
@@ -8,17 +8,22 @@ WOWPROGRESSURL = "http://www.wowprogress.com/guild/us/darkspear/Fluffy+Kitties"
 htmlFile = requests.get(WOWPROGRESSURL)
 htmlText = htmlFile.text
 
-# Use regex to find progress info and put into progress variable
-regex = 'class="innerLink ratingProgress".*<b>(\d+)'
-pattern = re.compile(regex)
+# Use regex to find progress info and put into progress variable. Comes from website formatted like '6/7 (H)'
+progressInfoRegex = 'class="innerLink ratingProgress" .*">\s?(\d+\/\d+\s?\(.\))'
+pattern = re.compile(progressInfoRegex)
 progress = re.findall(pattern, htmlText)
+progress = progress[0]
+
+# Use regex to strip out parentheses and space in progress
+progress = re.sub('[\s()]', '', progress)
+print(progress)
 
 # Create JSON data and push it to guilddata.json
 data = {}
-data['progress'] = progress[0]
+data['progress'] = progress
 
 
 with open("../public_html/json/guildprogress.json", "w") as outputfile:
     print(time.strftime("%b %d %Y %I:%M %p") +
-          " - Refreshing progress... Now " + progress[0] + "/13")
+          " - Refreshing progress... Now " + progress)
     json.dump(data, outputfile)
