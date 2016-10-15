591ee4d5e58144aa329c690a94c1802bfbf6ea5a
Jordan Barnes <rjordanbarnes@gmail.com>
2016-10-15 12:26:26 -0700

    Fixed guildprogress regex and updated Home

--- python/getguildprogress.py
+++ python/getguildprogress.py
@@ -9,14 +9,13 @@ htmlFile = requests.get(WOWPROGRESSURL)
 htmlText = htmlFile.text
 
 # Use regex to find progress info and put into progress variable. Comes from website formatted like '6/7 (H)'
-progressInfoRegex = 'class="innerLink ratingProgress" .*">\s?(\d+\/\d+\s?\(.\))'
+progressInfoRegex = 'class="innerLink ratingProgress" .*">.*(\d+\/\d+\s\(.\))'
 pattern = re.compile(progressInfoRegex)
 progress = re.findall(pattern, htmlText)
 progress = progress[0]
 
 # Use regex to strip out parentheses and space in progress
 progress = re.sub('[\s()]', '', progress)
-print(progress)
 
 # Create JSON data and push it to guilddata.json
 data = {}
